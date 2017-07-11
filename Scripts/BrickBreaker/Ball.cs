74c66e8a5fde75c8779c9bff1ebb563a37c54102
James Church <jcchurch@users.noreply.github.com>
2017-07-10 22:51:56 -0500

    Add files via upload

---
+++ Scripts/BrickBreaker/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float initialVelocity;
+    private Rigidbody rb;
+    private bool inPlay = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButton("Fire1") && inPlay == false)
+        {
+            transform.parent = null;
+            inPlay = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(this.initialVelocity, initialVelocity, 0));
+        }
+
+        if (transform.position.y < -4)
+        {
+            GameManager.instance.LoseLife();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Scripts/BrickBreaker/Brick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+    public GameObject particles;
+
+	void OnCollisionEnter(Collision other)
+    {
+        Instantiate(this.particles, transform.position, Quaternion.identity);
+        GameManager.instance.DestroyBrick();
+        Destroy(gameObject);
+    }
+}
