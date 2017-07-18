ad8845903130152d27c52ad0cf5c37712271753e
James Church <jcchurch@users.noreply.github.com>
2017-07-18 03:02:33 -0500

    Create Generator_Finished.cs

---
+++ Scripts/Elements/Generator_Finished.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (getSouthGem () == null) {
+			GameManager.instance.placeGem (transform.position.x, transform.position.y - 2);
+		}
+	}
+
+	private Collider getSouthGem() {
+		return getGem(new Vector3(transform.position.x, transform.position.y - 2));
+	}
+
+	private Collider getGem(Vector3 here) {
+		Collider[] gem = Physics.OverlapSphere (here, 0.5f);
+		if (gem.Length > 0) {
+			return gem [0];
+		}
+		return null;
+	}
+}
