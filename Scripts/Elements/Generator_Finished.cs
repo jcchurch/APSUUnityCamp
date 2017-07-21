6e82cd49178369a32e2f5bd82803d4979266657f
James Church <jcchurch@users.noreply.github.com>
2017-07-21 13:43:41 -0500

    Update Generator_Finished.cs

--- Scripts/Elements/Generator_Finished.cs
+++ Scripts/Elements/Generator_Finished.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour {
 
+	public Gem red;
+        public Gem black;
+        public Gem green;
+        public Gem blue;
+        public Gem pink;
+	
 	// Update is called once per frame
 	void Update () {
-		if (getSouthGem () == null) {
-			GameManager.instance.placeGem (transform.position.x, transform.position.y - 2);
-		}
+            if (getSouthGem () == null) {
+                float x = transform.position.x;
+                float y = transform.position.y - 2;
+                var gemList = new List<Gem>{ red, black, green, blue, pink };
+                Instantiate(gemList[Random.Range(0,gemList.Count)], new Vector3(x, y), Quaternion.identity);
+            }
 	}
 
 	private Collider getSouthGem() {
