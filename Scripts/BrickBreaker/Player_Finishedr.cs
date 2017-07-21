678af6e62c7c30f5e08b08cd0dd775082f501fce
James Church <jcchurch@users.noreply.github.com>
2017-07-21 14:13:17 -0500

    Update Player_Finishedr.cs

--- Scripts/BrickBreaker/Player_Finishedr.cs
+++ Scripts/BrickBreaker/Player_Finishedr.cs
@@ -10,16 +10,13 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        playerPosition = new Vector3(0, -3.5f, 0);
+            transform.position = new Vector3(0, -3.5f, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float xPosition = transform.position.x + (Input.GetAxis("Horizontal") * speed * Time.deltaTime);
-        xPosition = Mathf.Clamp(xPosition, -4f, 4f);
-
-        playerPosition = new Vector3(xPosition, playerPosition.y, playerPosition.z);
-
-        transform.position = playerPosition;
-    }
+            float xPosition = transform.position.x + (Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+            xPosition = Mathf.Clamp(xPosition, -4f, 4f);
+            transform.position = new Vector3(xPosition, playerPosition.y, playerPosition.z);
+        }
 }
