9a01d97084d5c697d7e036e2360023fc7473965e
James Church <jcchurch@users.noreply.github.com>
2017-07-21 16:54:23 -0500

    Update Bunny.cs

--- Scripts/FruitCatcher/Bunny.cs
+++ Scripts/FruitCatcher/Bunny.cs
@@ -11,21 +11,20 @@ public class Bunny : MonoBehaviour
     public GameObject potato;
 
     private int direction;
-    private float timeToDrop;
 
     // Use this for initialization
     void Start()
     {
         transform.position = new Vector3(0, 4f, 0);
         direction = 1;
-        timeToDrop = UnityEngine.Random.Range(3f, 6f);
+        Invoke("dropSomething", 3f);
     }
 
     // Update is called once per frame
     void Update()
     {
         float xPos = transform.position.x + (direction * speed * Time.deltaTime);
-        transform.position = new Vector3(Mathf.Clamp(xPos, -4f, 4f), transform.position.y, transform.position.z);
+        transform.position = new Vector3(xPos, transform.position.y, transform.position.z);
 
         if (transform.position.x >= 4f)
         {
@@ -36,14 +35,6 @@ public class Bunny : MonoBehaviour
         {
             direction = 1;
         }
-
-        if (timeToDrop <= 0)
-        {
-            dropSomething();
-            timeToDrop = UnityEngine.Random.Range(3f, 6f);
-        }
-
-        timeToDrop -= Time.deltaTime;
     }
 
     private void dropSomething()
@@ -55,5 +46,6 @@ public class Bunny : MonoBehaviour
         }
 
         Instantiate(toDrop, transform.position, Quaternion.identity);
+        Invoke("dropSomething", 3f);
     }
 }
