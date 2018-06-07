5c948ef7053d77d719e68be589d4e21680bc6fb5
James Church <jcchurch@users.noreply.github.com>
2018-06-06 23:51:26 -0500

    Update GameManager.cs

--- Scripts/Elements/GameManager.cs
+++ Scripts/Elements/GameManager.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
-    public static GameManager instance = null;
+	public GameObject generator;
+	public int movesLeft;
+
+
+	private Gem first;
+	private int score;
+
+	public static GameManager instance = null;
 
     // Use this for initialization
     void Start () {
@@ -20,6 +26,38 @@ public class GameManager : MonoBehaviour {
 			Destroy(gameObject);
 		}
 
+		for (int x = -7; x <= 7; x++) {
+			for (int y = -4; y <= 4; y++) {
+				Instantiate (generator, new Vector3 (x, y, 0), Quaternion.identity);
+			}
+		}
+	}
+
+	public void SetGem(Gem gem) {
+
+		if (gem != null && first == null)
+		{
+			first = gem;
+		}
+		else if (gem != null && first != null)
+		{
+			Vector3 firstLocal = first.transform.position;
+			first.SetPosition(gem.transform.position);
+			gem.SetPosition (firstLocal);
+
+			movesLeft--;
+			GameManager.instance.ScorePoints (-50);
+
+			first = null;
+		}
+	}
+
+	public void ScorePoints(int howmany) {
+		score = score + howmany;
+
+		if (movesLeft <= 0) {
+			Invoke ("Reset", 4f);
+		}
 	}
 
 	private void Reset() {
