5631af83684bb17c9f4cbeaf7277b405ce87560b
James Church <jcchurch@users.noreply.github.com>
2018-06-06 23:52:31 -0500

    Update Generator.cs

--- Scripts/Elements/Generator.cs
+++ Scripts/Elements/Generator.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour {
 
+	public Gem red;
+	public Gem black;
+	public Gem green;
+	public Gem blue;
+	public Gem pink;
+
+	void Start() {
+		InvokeRepeating ("DropItem", 0, 0.5f);
+	}
+
 	// Update is called once per frame
 	void Update () {
+		
 	}
 
-	private Collider getSouthGem() {
-		return getGem(new Vector3(transform.position.x, transform.position.y - 2));
-	}
-
-	private Collider getGem(Vector3 here) {
-		Collider[] gem = Physics.OverlapSphere (here, 0.5f);
-		if (gem.Length > 0) {
-			return gem [0];
+	void DropItem() {
+		Vector3 here = new Vector3 (transform.position.x, transform.position.y - 1, 0);
+		Collider[] gem = Physics.OverlapSphere (here, 0.25f);
+		if (gem.Length == 0) {
+			var gemList = new List<Gem>{ red, black, green, blue, pink };
+			Instantiate(gemList[Random.Range(0,gemList.Count)], here, Quaternion.identity);
 		}
-		return null;
 	}
 }
