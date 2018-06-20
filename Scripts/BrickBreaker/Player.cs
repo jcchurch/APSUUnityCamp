be49e6b8fa86e0076a082ca9bf5c709f96f9ea2e
James Church <jcchurch@users.noreply.github.com>
2018-06-20 01:05:54 -0500

    Update Player.cs

--- Scripts/BrickBreaker/Player.cs
+++ Scripts/BrickBreaker/Player.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
-    public float speed = 100f;
-
-    private Vector3 playerPosition;
+    public float speed = 5f;
 
 	// Use this for initialization
 	void Start () {
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () {
-            float x = transform.position.x + (Input.GetAxis("Horizontal") * speed * Time.deltaTime);
-            x = Mathf.Clamp(x, -4f, 4f);
-            transform.position = new Vector3(x, playerPosition.y, playerPosition.z);
-        }
+		float x = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		x = Mathf.Clamp (transform.position.x + x, -4.5f, 4.5f);
+		transform.position = new Vector3 (x, transform.position.y, 0);
+    }
 }
