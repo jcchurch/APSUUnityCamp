e16cb440436386610a61aade8354f17a56dcd875
James Church <jcchurch@users.noreply.github.com>
2018-06-20 01:04:42 -0500

    Update Ball.cs

--- Scripts/BrickBreaker/Ball.cs
+++ Scripts/BrickBreaker/Ball.cs
@@ -1,16 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour {
 
-    public float initialVelocity;
+    public float speed = 1000;
+
+	public int bricks = 12;
+	public int lives = 3;
+	public Text message;
+	public GameObject player;
+
     private Rigidbody rb;
     private bool inPlay = false;
 
     // Use this for initialization
     void Start () {
-		
+		Reset ();
 	}
 
     void Awake()
@@ -18,20 +25,44 @@ public class Ball : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
     }
 
+	public void Hit() {
+		bricks--;
+	}
+
+	void Clear() {
+		message.text = "";
+	}
+
     // Update is called once per frame
     void Update() {
-        if (Input.GetButton("Fire1") && inPlay == false)
+        if (Input.GetButton("Jump") && inPlay == false)
         {
             transform.parent = null;
             inPlay = true;
             rb.isKinematic = false;
-            rb.AddForce(new Vector3(this.initialVelocity, initialVelocity, 0));
+            rb.AddForce(new Vector3(speed, speed, 0));
         }
 
-        if (transform.position.y < -4)
+        if (transform.position.y < 2)
         {
-            GameManager.instance.LoseLife();
-            Destroy(gameObject);
+			lives--;
+			Reset ();
         }
+
+		if (lives == 0) 
+			message.text = "Game over!";
+
+		if (bricks == 0)
+			message.text = "You win!";
     }
+
+	public void Reset() {
+		inPlay = false;
+		rb.isKinematic = true;
+		message.text = "Lives: "+lives;
+		Invoke ("Clear", 3f);
+
+		transform.position = new Vector3 (player.transform.position.x, 3, 0);
+		transform.SetParent (player.transform);
+	}
 }
