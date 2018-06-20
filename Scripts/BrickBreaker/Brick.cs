5b42f24ce396b4d21d6f37f147c76324034ce1a8
James Church <jcchurch@users.noreply.github.com>
2018-06-20 01:05:28 -0500

    Update Brick.cs

--- Scripts/BrickBreaker/Brick.cs
+++ Scripts/BrickBreaker/Brick.cs
@@ -1,15 +1,16 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Brick : MonoBehaviour {
 
     public GameObject particles;
+	public Ball ball;
 
 	void OnCollisionEnter(Collision other)
     {
+		ball.Hit ();
         Instantiate(this.particles, transform.position, Quaternion.identity);
-        GameManager.instance.DestroyBrick();
         Destroy(gameObject);
     }
 }
